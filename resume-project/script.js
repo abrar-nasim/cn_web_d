37de5a4ffb1fb7f3ba6b69dd372837eaf398086e
Md Abrar Nasim <abrarnsim555@gmail.com>
2022-09-04 19:16:13 +0530

    add dynamic skill progress bar in resume project

--- resume-project/script.js
+++ resume-project/script.js
@@ -1,3 +1,5 @@
+// javascript to implement smooth scrolling
+
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 // console.log(navMenuAnchorTags);
 for(var i=0; i<navMenuAnchorTags.length; i++){
@@ -16,4 +18,50 @@ for(var i=0; i<navMenuAnchorTags.length; i++){
             window.scrollBy(0,50)
         },20);
     });
+}
+
+
+// javascript to implement dynamic skill progress
+
+let skillBarProgress = document.querySelectorAll(".skill-progress > div");
+let skillSection = document.getElementById("skills");
+
+window.addEventListener("scroll", checkScroll);
+let animationHit = false;
+
+initializeBars();
+
+function initializeBars(){
+    for(let i=0; i<skillBarProgress.length; i++){
+        skillBarProgress[i].style.width = 0 + "%"; 
+    }
+}
+
+function fillBars(){
+    for(let bar of skillBarProgress){
+        let currentWidth = 0;
+        let targetWidth = bar.getAttribute('data-bar-width');
+
+        let interval = setInterval(function(){
+            if(currentWidth >= targetWidth){
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + "%";
+
+        },5);
+    }
+}
+
+
+function checkScroll(){
+    let coordinates = skillSection.getBoundingClientRect();
+    if(!animationHit && coordinates.top <= window.innerHeight){
+        animationHit = true;
+        fillBars();
+    }else if(animationHit && coordinates.top > window.innerHeight){
+        animationHit = false;
+        initializeBars();
+    }
 }
